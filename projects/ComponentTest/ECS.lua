4c8a336039441754fa3e4a820ddb0b2cf11a5df8
paswa-dev <73659592+paswa-dev@users.noreply.github.com>
2024-05-14 10:33:35 -0400

    ECS

--- projects/ComponentTest/ECS.lua
+++ projects/ComponentTest/ECS.lua
@@ -1,8 +1,18 @@
 -- Component
-type System = (world) -> ()
-type SystemInfo = System | {
-    
+type SystemTable = {
+    system: (world: any) -> (),
+    priority: number?,
+    loop: string?,
 }
+
+type System = SystemTable | (world: any) -> ()
+
+type LoopInitializer = {
+    default: RBXScriptSignal,
+    [string]: RBXScriptSignal
+}
+
+local nullPtr = newproxy(false)
 
 local component = {}
 
@@ -21,25 +31,82 @@ function loop.new(world)
     })
 end
 
-function loop:scheduleSystem(sys: SystemInfo)
-    table.insert()
+function loop:_seperateSystemsByEvent(event_list)
+    local systems = {}
+    for _, system in next, self._systems do
+        local event = system["loop"] or "default"
+        if event_list[event] then
+            if systems[event] == nil then
+                systems[event] = {}
+            end
+            table.insert(systems[event], system["system"] or system)
+        end
+    end
+    return systems
 end
 
-function loop:scheduleSystems()
-
+function loop:begin(initalizer: LoopInitializer)
+    local systems = self:_seperateSystemsByEvent(initalizer)
+    for event_name, event_list in next, systems do
+        initalizer[event_name]:Connect(function()
+            for _, event in next, event_list do
+                event(self._world)
+            end
+        end)
+    end
 end
 
-function loop:removeSystem()
-
+function loop:scheduleSystem(system: System)
+    table.insert(self._systems, system)
 end
 
-function loop:replaceSystem()
+function loop:scheduleSystems(systems: {System})
+    for _, system in next, systems do
+        table.insert(self._systems, system)
+    end
+end
 
+function loop:removeSystem(system)
+    table.remove(self._systems, table.find(system))
+end
+
+function loop:replaceSystem(oldSystem, newSystem)
+    local index = table.find(self._systems, oldSystem)
+    if index then
+        self._systems[index] = newSystem
+    end
 end
 
 local world = {}
+-- Completely Isolate iterator function. Refining the search is fine, 
+-- but not creating an entire new table
 
 function world.new()
+    local self = {
+        entities = {},
+        componentToEntity = {},
+        nextId = 0,
+        idDeinstancedCache = {} -- Retains unused entites for recyling
+    }
+
+    return setmetatable(self, {__index = world})
+end
+
+function world:_recycleNextId()
+    if #idDeinstancedCache == 0 then
+        self.nextId += 1
+        return self.nextId
+    end
+    local recycleId = idDeinstancedCache[1]
+    task.spawn(table.remove, 1)
+    return recycleId
+end
+
+function world:query()
+    
+end
+
+function world:spawn()
 
 end
 
