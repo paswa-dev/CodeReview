54d0c7bd541b1f664a0b696d284e548b8b9bbe03
paswa-dev <73659592+paswa-dev@users.noreply.github.com>
2024-05-16 13:53:04 -0400

    Builder class

--- projects/ComponentTest/CSEntity.lua
+++ projects/ComponentTest/CSEntity.lua
@@ -1,10 +1,34 @@
 --// Collection Service Entity System
 local CS = game:GetService("CollectionService")
 local loop = {}
+
+local function component(tag_name: string)
+    local self = {}
+    tag_string = tag_name
+    data_table = {}
+
+    function self.assign(key, value)
+        data_table[key] = value
+    end
+
+    function self.build(object)
+        local folder = Instance.new("Folder")
+        folder.Name = tag_string
+
+        for key, value in next, data_table do
+            folder:SetAttribute(key, value)
+        end
+
+        folder.Parent = object
+    end
+    return self
+end
+
+
 local world = {
     id_number = 0,
-    entity_table = {} -- ID -> INSTANCE
-    trackedInstanceEntity_table = {} -- INSTANCE -> ID
+    trackedEntity_table = {} -- ID -> INSTANCE
+    trackedInstance_table = {} -- INSTANCE -> ID
 }
 --// Open/Closed Principle
 -- All functions must do only one thing. 
@@ -34,12 +58,22 @@ local function findOverlappingResults(...) -- Unoptimized probably?
     return results
 end
 
+local function buildComponents(object, ComponentBuilder_list)
+    for _, ComponentBuilder in next, ComponentBuilder_list do
+        ComponentBuilder.build(object)
+    end
+end
+
+local function hookDestructor(object, callback)
+    return object.Destroying:Connect(callback)
+end
+
 function world.instanceExist(instance)
-    return trackedInstanceEntity_table[instance] ~= nil
+    return trackedInstance_table[instance] ~= nil
 end
 
 function world.entityExist(id)
-    return entity_table[id] ~= nil
+    return trackedEntity_table[id] ~= nil
 end
 
 function world.incrementId()
@@ -48,36 +82,34 @@ function world.incrementId()
 end
 
 function world.findInstance(id)
-    return world.entity_table[id]
+    return world.trackedEntity_table[id]
 end
 
 function world.findEntity(instance)
-    return world.trackedInstanceEntity_table[instance]
+    return world.trackedInstance_table[instance]
 end
 
 function world.getEntityData(id)
-    return world.entity_table[id]
+    return world.trackedEntity_table[id]
 end
 
-function world.spawnAt(id, instance, kwargs)
-    world.entity_table[id] = {kwargs}
-    world.trackedInstanceEntity_table[instance]
+function world.wipe(instance)
+    local id = world.trackedEntity_table[instance]
+    world.trackedEntity_table[id] = nil
+    world.trackedEntity_table[instance] = nil
 end
 
-function world.spawn(instance, kwargs)
-    if world.entityExist[instance] then return end
+function world.spawn(instance, ComponentBuilder_list: {any})
     local id = world.incrementId()
-    world.spawnAt(id, instance, kwargs)
-end
-
-function world.query(...)
-    for i=0, select("#", ...) do
-        local instances = CS:GetTagged(select(i, ...))
-        findOverlappingResults(instances, ...)
+    if world.instanceExist(instance) == false then
+        world.trackedInstance_table[instance] = id
+        world.trackedEntity_table[id] = instance
+        buildComponents(instance, ComponentBuilder_list)
+        hookDestructor()
     end
 end
 
 return {
-    world = world,
-    loop = loop
+    World = world,
+    Loop = loop
 }
